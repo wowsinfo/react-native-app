be9472fd75c6c011c6a735621ca21768f179a1e7
Yiheng <development.henryquan@gmail.com>
2023-08-27 17:56:08 +1000

    fastlane setup and build for simulator

--- wowsinfo/index.js
+++ wowsinfo/index.js
@@ -3,7 +3,17 @@ import { AppRegistry, NativeModules } from 'react-native';
 import App from './src/wowsinfo';
 import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper';
 import './src/value/global';
-NativeModules.DevSettings.setIsDebuggingRemotely(true);
+
+if (__DEV__) {
+    NativeModules.DevSettings.setIsDebuggingRemotely(true);
+} else {
+    // clear up console
+    console.log = () => {};
+    console.warn = () => {};
+    console.error = () => {
+        // send to crashlytics instead
+    };
+}
 
 export function Main() {
     return (
