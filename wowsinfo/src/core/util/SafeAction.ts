37a19e30fe0863604b729c910c9e5bc5a498dc67
Yiheng <development.henryquan@gmail.com>
2023-08-29 21:58:56 +1000

    updated to ts

--- wowsinfo/src/core/util/SafeAction.ts
+++ wowsinfo/src/core/util/SafeAction.ts
@@ -1,8 +1,10 @@
+import { DefaultTheme } from 'react-native-paper';
 import {Actions} from 'react-native-router-flux';
 
 // Add safe action to check if that screen already exists in the stack
 // max is for the case where you need to push another same screen
-export const SafeAction = (screen, obj, max = 0) => {
+export const SafeAction = (screen: string, obj: any, max = 0) => {
+  // @ts-ignore
   if (Actions.state.routes.filter(r => r.routeName === screen).length > max) {
     console.log(`${screen} rejected`);
   } else {
