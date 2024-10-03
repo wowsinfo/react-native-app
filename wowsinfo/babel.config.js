168d9f5febec4991573731dbee9df8c105ec2e5d
Yiheng <development.henryquan@gmail.com>
2024-10-03 11:12:55 +1000

    upgrade project to 0.75.4

--- wowsinfo/babel.config.js
+++ wowsinfo/babel.config.js
@@ -6,12 +6,12 @@ module.exports = function (api) {
   ) {
     // remove console.* in production
     return {
-      presets: ['module:metro-react-native-babel-preset'],
+      presets: ['module:@react-native/babel-preset'],
       plugins: ['react-native-paper/babel', 'transform-remove-console'],
     };
   } else {
     return {
-      presets: ['module:metro-react-native-babel-preset'],
+      presets: ['module:@react-native/babel-preset'],
     };
   }
 };
